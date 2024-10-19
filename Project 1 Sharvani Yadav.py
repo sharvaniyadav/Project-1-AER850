185e3f03d69280b4d48f018f2fa321c5946ef63e
Sharvani Yadav <s1yadav@torontomu.ca>
2024-10-19 14:06:43 -0400

    Update Project 1 Sharvani Yadav.py

--- Project 1 Sharvani Yadav.py
+++ Project 1 Sharvani Yadav.py
@@ -23,6 +23,7 @@ from sklearn.ensemble import RandomForestClassifier                             
 from sklearn.svm import SVC                                                                        # Support Vector Classifier (SVC) is a classification algorithm based on finding a decision boundary between classes.
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score                # These metrics help evaluate the performance of classification models.
 import joblib                                                                                      # Joblib is used to save and load models, making it easier to persist and reuse trained models.
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -100,7 +101,7 @@ improving its ability to predict accurately for all steps, not just the ones wit
 
 my_splitter = StratifiedShuffleSplit(n_splits = 1,                             # Data Splits Only Once    
                                      test_size = 0.2,                          # 20% Data for Testing  
-                                     random_state = 42)                        # Self Explanatory 
+                                     random_state = 42)                        # Self Explanatory :D
 
 for train_index, test_index in my_splitter.split(df, df["Step"]):
     strat_df_train = df.loc[train_index].reset_index(drop=True)
@@ -111,6 +112,30 @@ coord_train = strat_df_train.drop("Step", axis = 1)
 step_train = strat_df_train["Step"]
 coord_test = strat_df_test.drop("Step", axis = 1)
 step_test = strat_df_test["Step"]
+
+
+'''Prepping the Dataset:  '''
+
+coord_scaler = StandardScaler()                                                # Create a scaler to standardize the data by removing the average and adjusting the scale.
+
+coord_scaler.fit(coord_train)                                                  # Fit the scaler to the training data so it learns the average and spread of the features.
+
+scaled_data_train = coord_scaler.transform(coord_train)                        # Use the fitted scaler to transform the training data, applying the standardization.
+
+scaled_data_train_df = pd.DataFrame(scaled_data_train,                         # Convert the scaled training data back into a DataFrame to keep the same column names.
+                                    columns = coord_train.columns)
+coord_train = scaled_data_train_df                                             # Update the original training data variable to now hold the scaled data.
+
+scaled_data_test = coord_scaler.transform(coord_test)                          # Apply the same scaling to the test data using the same parameters learned from the training data.
+
+scaled_data_test_df = pd.DataFrame(scaled_data_test,                           # Convert the scaled test data into a DataFrame, preserving the column names.
+                                   columns = coord_test.columns)
+coord_test = scaled_data_test_df                                               # Update the original test data variable to now hold the scaled data.
+
+'''The standard deviation of the three features differs significantly, with X being 
+more spread out than Z, which means X could have a greater impact on the model.Although the 
+values aren't vastly different, this could still influence the model's performance.Therefore,
+it’s a good idea to apply scaling methods to improve the model's effectiveness.'''
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -127,7 +152,7 @@ too similar, so none of the data needs to be dropped from the dataset itself.'''
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-"(2.4) STEP 4: Classification Model Development/Engineering Marks"
+"(2.4) STEP 4: Classification Model Development/Engineering"
 
 "Training Model 1 - LOGISTIC REGRESSION"
 
@@ -139,7 +164,6 @@ train_predictions = log_reg_model.predict(coord_train)
 print("Classification Report for Training Set \n", classification_report(step_train, 
                                                                          train_predictions, 
                                                                          zero_division=0))
-
 # Predictions on test data
 test_predictions = log_reg_model.predict(coord_test)
 print("Classification Report for Test Set \n", classification_report(step_test, 
@@ -148,22 +172,81 @@ print("Classification Report for Test Set \n", classification_report(step_test,
 
 "Training Model 2 - RANDOM FOREST"
 
-rf_classifier = RandomForestClassifier()
+rf_model = RandomForestClassifier(random_state=42, 
+                                       max_depth=5, 
+                                       min_samples_split=45,
+                                       min_samples_leaf=40,
+                                       n_estimators=10, 
+                                       max_features='sqrt',
+                                       class_weight='balanced')
 
-# Train the model
-rf_classifier.fit(coord_train, step_train)
+rf_model.fit(coord_train, step_train)                                                         # Train the model
 
-# Predictions on training data
-rf_train_predictions = rf_classifier.predict(coord_train)
-print("Classification Report for Training Set \n", classification_report(step_train, 
+rf_train_predictions = rf_model.predict(coord_train)                                          # Predictions on training data
+print("Classification Report for Training Set \n", classification_report(step_train,          # Print the classification report for the training set
                                                                          rf_train_predictions, 
                                                                          zero_division=0))
 
-# Predictions on test data
-rf_test_predictions = rf_classifier.predict(coord_test)
-print("Classification Report for Test Set \n", classification_report(step_test, 
+rf_test_predictions = rf_model.predict(coord_test)                                            # Predictions on test data
+print("Classification Report for Test Set \n", classification_report(step_test,               # Print the classification report for the testing set
                                                                      rf_test_predictions, 
-                                                                     zero_division=0))
+                                                                 zero_division=0))
+# Hyperparameter Grid for Tuning
+param_grid_rf = {
+     'n_estimators': [10, 30, 50],
+     'max_depth': [None, 10, 20, 30],
+     'min_samples_split': [2, 5, 10],
+     'min_samples_leaf': [1, 2, 4],
+     'max_features': ['sqrt', 'log2']
+ }
+
+# Implement Grid Search for Hyperparameter Tuning
+grid_search_rf = GridSearchCV(estimator=rf_model, param_grid=param_grid_rf, cv=5, scoring='f1_weighted', n_jobs=1)
+grid_search_rf.fit(coord_train, step_train)
+
+# Retrieve the best hyperparameters from the grid search
+best_params_rf = grid_search_rf.best_params_
+print("Best Hyperparameters for Random Forest Model:", best_params_rf)
+
+# Get the best model from grid search
+best_rf_model = grid_search_rf.best_estimator_
+
+# Make predictions with the best model on the test set
+final_predictions = best_rf_model.predict(coord_test)
+
+
+# Performance Analysis
+accuracy_score_rf = accuracy_score(step_test, final_predictions)
+confusion_matrix_rf = confusion_matrix(step_test, final_predictions)
+classification_report_rf = classification_report(step_test, final_predictions)
+
+# Print accuracy score and classification report
+print("Model Performance Analysis: Random Forest\n")
+print("Accuracy Score:", accuracy_score_rf)
+print("\nClassification Report:\n", classification_report_rf)
+
+# Plot the confusion matrix
+disp_rf = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix_rf)
+disp_rf.plot(cmap=plt.cm.Blues)
+plt.title("Confusion Matrix for Random Forests")
+plt.show()
+
+sb.heatmap(confusion_matrix_rf)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
